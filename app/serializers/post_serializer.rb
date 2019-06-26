55c5fe71546afc4b6581149527739827d9175241
Nicolas Marini <nmarini44@gmail.com>
2019-06-26 23:13:14 +0000

    Automatically backed up by Learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,13 +1,13 @@
 class PostSerializer
   def self.serialize(post)
- 
+
 
     serialized_post = '{'
- 
+
     serialized_post += '"id": ' + post.id.to_s + ', '
     serialized_post += '"title": "' + post.title + '", '
     serialized_post += '"description": "' + post.description + '", '
- 
+
 
     serialized_post += '"author": {'
     serialized_post += '"name": "' + post.author.name + '"}'
